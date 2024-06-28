8c0a18cdbb0b54851e8b3f9da4a3a119944156a1
afsalahamed07 <afsalahamed07@gmail.com>
2024-06-28 14:04:43 +0530

    document formatting

--- Book.js
+++ Book.js
@@ -2,53 +2,55 @@ const myLibrary = [];
 let bookCounter = 0; // counter is used to index the books
 
 // some random books
-const book1 = new Book( "The Hobbit", "J.R.R. Tolkien", 295, true );
-const book2 = new Book( "The Lord of the Rings", "J.R.R. Tolkien", 1178, false );
-const book3 = new Book( "The Silmarillion", "J.R.R. Tolkien", 365, true );
+const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
+const book2 = new Book("The Lord of the Rings", "J.R.R. Tolkien", 1178, false);
+const book3 = new Book("The Silmarillion", "J.R.R. Tolkien", 365, true);
 
-addBookToLibrary( book1 );
-addBookToLibrary( book2 );
-addBookToLibrary( book3 );
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
 
 renderLibrary();
 
-function Book( title, author, pages, read ) {
-    this.id = ++bookCounter;
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.id = ++bookCounter;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
 
-    this.info = function () {
-        return `${ this.title } by ${ this.author }, ${ this.pages } pages, ${ this.read ? "read" : "not read yet" }`;
+        this.info = function() {
+            return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read yet"}`;
+        };
     }
 }
 
-function addBookToLibrary( book ) {
-    console.log( `adding ${ book.title } to library` )
-    myLibrary.push( book );
+function addBookToLibrary(book) {
+  console.log(`adding ${book.title} to library`);
+  myLibrary.push(book);
 }
 
-function removeBookFromLibrary( book ) {
-    const index = myLibrary.indexOf( book );
-    if ( index !== -1 ) {
-        myLibrary.splice( index, 1 );
-    }
+function removeBookFromLibrary(book) {
+  const index = myLibrary.indexOf(book);
+  if (index !== -1) {
+    myLibrary.splice(index, 1);
+  }
 }
 
-function renderBook( book ) {
-    return `<div id= "book-${ book.id }" class="book flex flex-row gap-4 shadow-md rounded-md">
-        <div class="basis-1/3 p-4 font-bold">${ book.title }</div>
-        <div class="basis-1/4 p-4">${ book.author }</div>
-        <div class="basis-1/4 p-4">${ book.pages }</div>
+function renderBook(book) {
+  return `<div id= "book-${book.id}" class="book flex flex-row gap-4 shadow-md rounded-md">
+        <div class="basis-1/3 p-4 font-bold">${book.title}</div>
+        <div class="basis-1/4 p-4">${book.author}</div>
+        <div class="basis-1/4 p-4">${book.pages}</div>
         <div class="basis-1/4 p-4 flex flex-row gap-2">
             <p class="status basis-2/3">
-                ${ book.read ? "read" : "not read yet" }
+                ${book.read ? "read" : "not read yet"}
             </p>
             <label class="tems-center cursor-pointer">
-                  <input ${ book.read ? checked = "checked" : "" } type="checkbox" value="" class="status-check sr-only peer">
-                  <div class="relative w-11 h-6 bg-red-600 peer-focus:outline-none 
-                   rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full 
+                  <input ${book.read ? (checked = "checked") : ""} type="checkbox" value="" class="status-check sr-only peer">
+                  <div class="relative w-11 h-6 bg-red-600 peer-focus:outline-none
+                   rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full
                    peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
             </label>
         </div>
@@ -60,14 +62,13 @@ function renderBook( book ) {
 }
 
 function renderLibrary() {
-    console.log( "renderLibrary" )
-    myLibrary.map( book => {
-        console.log( "book", book )
-        document.getElementById( "library" ).innerHTML += renderBook( book );
-    } );
+  console.log("renderLibrary");
+  myLibrary.map((book) => {
+    console.log("book", book);
+    document.getElementById("library").innerHTML += renderBook(book);
+  });
 }
 
-function appendBookToLibrary( book ) {
-    document.getElementById( "library" ).innerHTML += renderBook( book );
+function appendBookToLibrary(book) {
+  document.getElementById("library").innerHTML += renderBook(book);
 }
-
--- script.js
+++ script.js
@@ -1,61 +1,60 @@
-const dialog = document.querySelector( "dialog" );
-const addButton = document.querySelector( "#add-book" );
-const closeButton = document.querySelector( "#close-dialog" );
-const addBookForm = document.querySelector( "#add-book-form" );
-const removeBookButtons = document.querySelectorAll( ".remove-btn" );
-const statusCheckboxes = document.querySelectorAll( ".status-check" );
+const dialog = document.querySelector("dialog");
+const addButton = document.querySelector("#add-book");
+const closeButton = document.querySelector("#close-dialog");
+const addBookForm = document.querySelector("#add-book-form");
+const removeBookButtons = document.querySelectorAll(".remove-btn");
+const statusCheckboxes = document.querySelectorAll(".status-check");
 
-addButton.addEventListener( "click", () => {
-    dialog.showModal();
-} );
+addButton.addEventListener("click", () => {
+  dialog.showModal();
+});
 
-closeButton.addEventListener( "click", () => {
-    dialog.close();
-} );
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
 
-addBookForm.addEventListener(
-    "click",
-    ( event ) => {
-        event.preventDefault();
-        const title = document.querySelector( "#title" ).value;
-        const author = document.querySelector( "#author" ).value;
-        const pages = document.querySelector( "#pages" ).value;
-        const read = document.querySelector( "#read" ).checked;
-        const book = new Book( title, author, pages, read );
-        addBookToLibrary( book );
-        appendBookToLibrary( book );
-        dialog.close();
-    }
+addBookForm.addEventListener("click", (event) => {
+  event.preventDefault();
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
+  const read = document.querySelector("#read").checked;
+  const book = new Book(title, author, pages, read);
+  addBookToLibrary(book);
+  appendBookToLibrary(book);
+  dialog.close();
+});
+
+removeBookButtons.forEach((button) =>
+  button.addEventListener("click", () => {
+    console.log("removeBookButton");
+
+    // grab the parent div
+    let parentDiv = button.parentElement.parentElement;
+
+    let id = parentDiv.id;
+
+    // remove the book from the DOM
+    parentDiv.remove();
+
+    // remove the book from the library
+    const book = myLibrary.find((book) => `book-${book.id}` === id);
+    removeBookFromLibrary(book);
+  }),
 );
 
-removeBookButtons.forEach( button => button.addEventListener
-     ( "click", () => {
-        console.log( "removeBookButton" );
+statusCheckboxes.forEach((checkbox) =>
+  checkbox.addEventListener("click", () => {
+    let parentDiv = checkbox.parentElement.parentElement;
+    let id = parentDiv.id;
 
-        // grab the parent div
-        let parentDiv = button.parentElement.parentElement;
+    let status = parentDiv.querySelector(".status");
 
-        let id = parentDiv.id;
+    checkbox.checked
+      ? (status.textContent = "read")
+      : (status.textContent = "not read yet");
 
-        // remove the book from the DOM
-        parentDiv.remove();
-
-        // remove the book from the library
-        const book = myLibrary.find( book => `book-${ book.id }` === id );
-        removeBookFromLibrary( book );
-    } )
+    const book = myLibrary.find((book) => `book-${book.id}` === id);
+    book.read = !book.read;
+  }),
 );
-
-statusCheckboxes.forEach( checkbox => checkbox.addEventListener(
-    "click",
-    () => {
-        let parentDiv = checkbox.parentElement.parentElement;
-        let id = parentDiv.id;
-
-        let status = parentDiv.querySelector( ".status" );
-        checkbox.checked ? status.textContent = "read" : status.textContent = "not read yet";
-
-        const book = myLibrary.find( book => `book-${ book.id }` === id );
-        book.read = !book.read;
-    })
-)
